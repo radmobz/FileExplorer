e4fff6cbe74cf2090d3be6749c41a26885eb181a
Julien <jjuge.julien@gmail.com>
2016-02-27 11:36:52 +0100

    Initialisation du repo

---
+++ src/main/java/com/julien/alexis/dev/FileExplorerApplication.java
@@ -0,0 +1,12 @@
+package com.julien.alexis.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileExplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileExplorerApplication.class, args);
+	}
+}
